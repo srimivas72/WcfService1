4526d0a86f5276bb91513a17d4641a0cf12e3790
skottapalli <skottapalli@MOBILE96>
2023-06-23 10:29:19 -0400

    Commit 1

--- IService1.cs
+++ IService1.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
-using System.ServiceModel;
+using System.ServiceModel;anxkanka
 using System.ServiceModel.Web;
 using System.Text;
 
